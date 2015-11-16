d4be124be511cf39aebfc795d3eff8622bb1b890
shitsecurity <shitsecurity@ya.ru>
2015-11-16 10:22:11 +0000

    +refactor

--- data/scripts/kernel.sh
+++ data/scripts/kernel.sh
@@ -1,34 +1,39 @@
 echo "[*] kernel: `uname -a`"
 
-if [ -d /proc ]
-then echo '[+] /proc exists'
-    proc=true
-else echo '[-] /proc disabled'
-    proc=false
-fi
+recovered=false
+unknown='unknown'
 
-skip=false
-sysctl=`whereis sysctl|awk '{print $2}'`
-if [ -z $sysctl ]
-then echo '[!] sysctl not found'
-    if [ $proc = true ]
-    then echo '[*] recovering via /proc'
-        mmap_min_addr=`cat /proc/sys/vm/mmap_min_addr`
+if [ -d /proc ]
+then echo '[+] /proc enabled'
+    if [ ${recovered} = false ]
+    then mmap_min_addr=`cat /proc/sys/vm/mmap_min_addr`
         modprobe=`cat /proc/sys/kernel/modprobe`
         kptr=`cat /proc/sys/kernel/kptr_restrict`
         mod_disabled=`cat /proc/sys/kernel/modules_disabled`
-    else 'echo [-] access denied'
-        skip=true
+        smep=`grep smep /proc/cpuinfo`
+        recovered=true
     fi
-else echo "[+] sysctl: $sysctl" 
-    mmap_min_addr=`${sysctl} vm.mmap_min_addr|awk '{print $3}'`
-    modprobe=`${sysctl} kernel.modprobe|awk '{print $3}'`
-    kptr=`${sysctl} kernel.kptr_restrict|awk '{print $3}'`
-    mod_disabled=`${sysctl} kernel.modules_disabled|awk '{print $3}'`
+else echo '[-] /proc disabled'
 fi
 
-if [ $skip = false ]
-then if [ $kptr -eq 0 ]
+sysctl=`whereis sysctl|awk '{print $2}'`
+if [ -z "${sysctl}" ]
+then echo '[-] sysctl not found'
+else echo "[+] sysctl: ${sysctl}"
+    if [ ${recovered} = false ]
+    then mmap_min_addr=`${sysctl} vm.mmap_min_addr|awk '{print $3}'`
+        modprobe=`${sysctl} kernel.modprobe|awk '{print $3}'`
+        kptr=`${sysctl} kernel.kptr_restrict|awk '{print $3}'`
+        mod_disabled=`${sysctl} kernel.modules_disabled|awk '{print $3}'`
+        smep=${unknown}
+        recovered=true
+    fi
+fi
+
+if [ $recovered = true ]
+then echo "[*] modprobe: ${modprobe}"
+
+    if [ $kptr -eq 0 ]
         then echo '[+] /proc/kallsyms enabled'
         else echo '[-] kallsyms disabled'
     fi
@@ -38,11 +43,16 @@ then if [ $kptr -eq 0 ]
         else echo "[-] mmap_min_addr ${mmap_min_addr}"
     fi
 
-    echo "[*] modprobe: ${modprobe}"
-
     if [ $mod_disabled -eq 0 ]
         then echo '[+] lkm enabled'
         else echo '[-] lkm disabled'
+    fi
+
+    if [ -z "$smep" ]
+        then echo '[+] smep disabled'
+    elif [ "$smep" = ${unknown} ]
+        then  echo '[!] smep status unknown'
+    else echo '[-] smep enabled'
     fi
 fi
 
